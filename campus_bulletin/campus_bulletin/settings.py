85326adb7c66d3595425b3b410aa468b183ec018
Jerome Maunga <jerhomemaunga@gmail.com>
2025-09-29 17:26:52 +0300

    Changed the settings to incorporate MySQL database

--- campus_bulletin/campus_bulletin/settings.py
+++ campus_bulletin/campus_bulletin/settings.py
@@ -74,8 +74,12 @@ WSGI_APPLICATION = 'campus_bulletin.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'campus_bulletin',
+        'USER': 'root',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
