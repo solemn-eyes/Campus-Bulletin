222d6d5051fd17f49acad46be8615e92107a8def
Jerome Maunga <jerhomemaunga@gmail.com>
2025-09-30 21:33:01 +0300

    Adding the accounts.url

--- campus_bulletin/campus_bulletin/urls.py
+++ campus_bulletin/campus_bulletin/urls.py
@@ -15,8 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.http import HttpResponse
+
+def home_view(request):
+    return HttpResponse("Welcome to the Campus Bulletin Board!")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home_view, name='home'),
+    path('', include('accounts.urls')),
 ]
