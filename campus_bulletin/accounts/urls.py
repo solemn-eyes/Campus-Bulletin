fe766fcb3e2f0db3dfa0b27bca4faf7c1e1964f6
Jerome Maunga <jerhomemaunga@gmail.com>
2025-09-30 21:37:56 +0300

    Implementing the account's urls

---
+++ campus_bulletin/accounts/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("signup/", views.student_signup, name="student_signup"),
+    path("login/", views.student_login, name="student_login"),
+    path("logout/", views.student_logout, name="student_logout"),
+]
