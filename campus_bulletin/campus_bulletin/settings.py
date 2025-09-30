96f1660c4a70b682df3a032723d39abde6ad1a26
Jerome Maunga <jerhomemaunga@gmail.com>
2025-09-30 20:39:03 +0300

    Adding the apps and accounts.student

--- campus_bulletin/campus_bulletin/settings.py
+++ campus_bulletin/campus_bulletin/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts',
+    'bulletin',
 ]
 
 MIDDLEWARE = [
@@ -124,3 +126,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Custom user model
+AUTH_USER_MODEL = 'accounts.Student'
+
+
