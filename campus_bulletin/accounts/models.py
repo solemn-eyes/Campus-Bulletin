8a3093a831e485ad8b174614ec750a43c5008ffc
Jerome Maunga <jerhomemaunga@gmail.com>
2025-10-02 11:36:41 +0300

    Updates on the model

--- campus_bulletin/accounts/models.py
+++ campus_bulletin/accounts/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser, BaseUserManager
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
 # Creating models for the Students Login/Signup
 
@@ -20,12 +20,12 @@ class StudentManager(BaseUserManager):
         student.save(using=self._db)
         return student
     
-class Student(AbstractUser):
+class Student(AbstractBaseUser):
     admission_number = models.CharField(max_length=50, unique=True)
     full_name = models.CharField(max_length=100)
     course = models.CharField(max_length=100, blank=True, null=True)
     year_of_study = models.IntegerField(blank=True, null=True)
-    school = models.CharField(max_length=100, blank=True, null=True)
+    faculty = models.CharField(max_length=100, blank=True, null=True)
     is_active = models.BooleanField(default=True)
     is_admin = models.BooleanField(default=False)
 
